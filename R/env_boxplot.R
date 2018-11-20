8e0f5d4387834f601c27db604696ba4477103c15
Sven Rubanschi <sven_rubi@hotmail.de>
2018-11-20 18:47:38 +0100

    grouped boxplot function

---
+++ R/env_boxplot.R
@@ -0,0 +1,37 @@
+#' Not working yet but soon
+#' 
+#' @export
+env_boxplot <- function(env, group.col = 0, col.sel){
+  if (group.col == 0){
+    stop('column of the groups is missing')
+  } else {
+    group <- env[group.col]
+    env <- env[-c(group.col)]
+  }
+  if (exists("col.sel") == T){
+    env <- env[col.sel] # important to write in the help the selection is like no group is in the data set
+    env_Obs <- colnames(env)
+  } else {
+    env_Obs <- colnames(env)
+  }
+  X <- ncol(env)
+  result <- list()
+  for(i in 1:X) {
+    use <- data.frame(env[i], group)
+    colnames(use) <- c("Var", "Group")
+    out <- PMCMR::posthoc.kruskal.dunn.test(Var~Group,
+                                     data = use,
+                                     p.adjust="bonf")
+    out.p <- PMCMR::get.pvalues(out)
+    out.mcV <- multcompView::multcompLetters(out.p, threshold = 0.05)
+    letter <- data.frame(out.mcV$Letters)
+    a <-boxplot(use$Var~use$Group, ylab = env_Obs[i],
+                ylim=c(min(use$Var, na.rm = T) , 1.1*max(use$Var, na.rm = T))
+                ,las=1)
+    over <- 0.1*max( a$stats[nrow(a$stats),] )
+    text(c(1:nlevels(use$Group)) , a$stats[nrow(a$stats),]+over , letter[,1])
+    result[[i]] <- c(env_Obs[i],out.p)
+    result_p.value <- do.call(rbind, result)
+  }
+  return(result_p.value)
+}
